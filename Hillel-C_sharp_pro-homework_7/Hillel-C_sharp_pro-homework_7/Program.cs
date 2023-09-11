33e5bc3b01b9c8e813d55d9aa679e4e70d2aab88
Oleksandr Cherevko <oc.oleks.cherevko@gmail.com>
2023-09-11 05:40:23 +0300

    task done

--- Hillel-C_sharp_pro-homework_7/Hillel-C_sharp_pro-homework_7/Program.cs
+++ Hillel-C_sharp_pro-homework_7/Hillel-C_sharp_pro-homework_7/Program.cs
@@ -1,2 +1,75 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class BarberShop
+{
+    static SemaphoreSlim barberSemaphore = new SemaphoreSlim(0);
+    static SemaphoreSlim customerSemaphore = new SemaphoreSlim(0); 
+    static object chairLock = new object();
+    static int availableChairs = 5; 
+
+    static void Main()
+    {
+        Thread barberThread = new Thread(Barber);
+        barberThread.Start();
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Thread customerThread = new Thread(Customer);
+            customerThread.Start(i);
+            Thread.Sleep(1000);
+        }
+
+        Console.ReadLine();
+    }
+
+    static void Barber()
+    {
+        while (true)
+        {
+            Console.WriteLine("Barber is sleeping...");
+
+            customerSemaphore.Wait(); 
+            lock (chairLock)
+            {
+                availableChairs++;
+            }
+
+            Console.WriteLine("Barber is cutting hair");
+
+            Thread.Sleep(3000); 
+
+            barberSemaphore.Release();
+        }
+    }
+
+    static void Customer(object customerId)
+    {
+        int id = (int)customerId;
+        bool gotHaircut = false;
+
+        lock (chairLock)
+        {
+            if (availableChairs > 0)
+            {
+                availableChairs--;
+
+                Console.WriteLine($"Customer {id} is waiting in the waiting room.");
+
+                customerSemaphore.Release();
+                gotHaircut = true;
+            }
+            else
+            {
+                Console.WriteLine($"Customer {id} left because there are no available chairs.");
+            }
+        }
+
+        if (gotHaircut)
+        {
+            barberSemaphore.Wait();
+
+            Console.WriteLine($"Customer {id} got a haircut and leaves the barber shop.");
+        }
+    }
+}
+
+
+
